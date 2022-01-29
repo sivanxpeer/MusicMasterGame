96df268b9e6bbc77855c8e54fac71f01135376e7
sivanxpeer <sivan.x.peer@gmail.com>
2022-01-29 10:48:06 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+app.get("/", (req, res) => {res.send("hello")})
+
+app.listen(port,()=>{console.log('listening on port',port)})
