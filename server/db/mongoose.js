dc2efb69d579c7f675f184c77202b904d35058cf
sivanxpeer <sivan.x.peer@gmail.com>
2022-02-01 21:25:49 +0200

    dotenv config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const PASSWORD = process.env.PASSWORD;
-// console.log(PASSWORD);
-// mongoose.connect(`mongodb+srv://psivi92:${PASSWORD}@cluster0.ltcww.mongodb.net/songsDB?retryWrites=true&w=majority`).then(()=>{console.log("connected")}).catch((err)=>{console.log(err)})
-
-// mongoose.connect(`mongodb+srv://psivi92:G,Bsb48TKQ,DbY-@cluster0.ltcww.mongodb.net/songsDB?retryWrites=true&w=majority`,()=>{
-//     console.log("connected to db");
-// })
-
-
 const URI = process.env.URI;
 mongoose.connect(`${URI}`).then(()=>{console.log("connected to db")}).catch((err)=>{console.log(err)});
