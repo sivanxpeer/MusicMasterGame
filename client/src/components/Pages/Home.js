32f41b67532c7681842dc0b1d08f05f4609ee400
sivanxpeer <sivan.x.peer@gmail.com>
2022-02-08 22:45:07 +0200

    register

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
+// import Login from "../login/Login";
+import Register from "../Register";
 
 const Home = () => {
   return <div>
     <h1>MUSIC MASTER</h1>
-    <ul className="nav-menu">
-      <li className="nav-item">
-        <NavLink exact to="/login" className="nav-links" activeClassName='active'>
-          login
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink exact to="/signup" className="nav-links" activeClassName='active'>
-          signup
-        </NavLink>
-      </li>
-    </ul>
-    {/* <Link to="/register" exact component={Register}></Link> */}
-    {/* <Register></Register> */}
+    <Register></Register>
+    <button className="btn">
+      <NavLink exact to="/login" className="nav-links" activeClassName='active'>
+        login
+      </NavLink>
+    </button>
+    <button className="btn">
+      <NavLink exact to="/signup" className="nav-links" activeClassName='active'>
+        signup
+      </NavLink>
+    </button>
+
+
   </div>;
 };
 
